94a694b9a9870d2123daf597f04cbb193b9bfc4e
Matheus Oliveira <contato.mths@outlook.com>
2024-02-27 15:14:16 -0300

    add dobradiças

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -1,7 +1,7 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer"
 
 export const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   from: process.env.SENDER_EMAIL,
   auth: {
     user: process.env.SENDER_EMAIL,
@@ -10,4 +10,4 @@ export const transporter = nodemailer.createTransport({
   tls: {
     rejectUnauthorized: false,
   },
-});
+})
